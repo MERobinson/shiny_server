3fa760a81688e307fd5221b5c12e8a4914cd937e
MERobinson <mark.e.robinson.45@gmail.com>
2020-02-15 16:20:47 -0800

    scwb

--- app_scwb/app.R
+++ app_scwb/app.R
@@ -24,12 +24,16 @@ gen_pptx <- function(plot, file, height = 5, width = 5, left = 1, top = 1) {
         print(target = file)
 }
 
+# load internal datasets
+internal_dat <- list.files("data", pattern = ".xlsx", full.names = T)
+names(internal_dat) <- sub("^data/(.+)\\.xlsx", "\\1", internal_dat)
+
 #################################### UI #######################################
 ui <- function(request) {
     fluidPage(
         
         # title
-        title = "Prism", 
+        title = "scWB", 
         theme = shinytheme("flatly"),
         
         titlePanel(tags$h3(tags$a(
@@ -39,63 +43,25 @@ ui <- function(request) {
         
         sidebarLayout(
             sidebarPanel(width = 3,
+                         selectInput(inputId, label, choices, selected = NULL, multiple = FALSE,
+                                     selectize = TRUE, width = NULL, size = NULL)
+                         selectInput(inputId = "existing_dat",
+                                     label = "Choose a dataset:",
+                                     choice = names(internal_df)),
                          fileInput(inputId = "input_file",
-                                   label = "Choose a file:",
+                                   label = "Upload an excel file:",
                                    multiple = F,
-                                   accept = c(".pzfx", ".csv", ".xlsx")),
-                         div(style="display:inline-block;",
-                             radioGroupButtons(inputId = "plot_type", 
-                                               label = "Choose plot type:",
-                                               choices = list("Dotplot" = "dotplot",
-                                                              "Violin" = "violin",
-                                                              "Boxplot" = "boxplot"),
-                                               justified = T,
-                                               size = 'xs',
-                                               width = '100%',
-                                               status = "default",
-                                               selected = "dotplot")),
-                         div(style="display:inline-block;",
-                             radioGroupButtons(inputId = "stat_choice",
-                                               label = "Choose statistical test:",
-                                               choices = list("T-test" = "ttest",
-                                                              "ANOVA" = "anova"),
-                                               justified = T,
-                                               size = 'xs',
-                                               width = '100%',
-                                               status = "default",
-                                               selected = "anova")),
-                         div(style="display:inline-block;",
-                             radioGroupButtons(inputId = "sig_style",
-                                               label = "Choose style of significance labelling:",
-                                               choices = list("Bars" = "bars",
-                                                              "Heatmap" = "heatmap",
-                                                              "Effsize" = "effect_size"),
-                                               justified = T,
-                                               size = 'xs',
-                                               width = '100%',
-                                               status = "default",
-                                               selected = "bars")),
-                         div(style="display:inline-block;",
-                             radioGroupButtons(inputId = "control_type",
-                                               label = "Select control type:",
-                                               choices = list("One-v-All" = "single",
-                                                              "Paired" = "paired"),
-                                               justified = T,
-                                               size = 'xs',
-                                               width = '100%',
-                                               status = "default",
-                                               selected = "single")),
+                                   accept = c(".xlsx")),
                          div(style="display:inline-block;width:45%;", 
-                             colourInput("col_selected","Selected", value = "#a00000",
+                             colourInput("col_selected","Low", value = "#202080",
                                          allowTransparent = TRUE)),
                          div(style="display:inline-block;width:45%;", 
-                             colourInput("col_other","Others", value = "#007000",
+                             colourInput("col_other","High", value = "#000000",
                                          allowTransparent = TRUE)),
                          textInput("ylab_text", "Y-axis label:"),
-                         textInput("selected_cond_label", "Selected condition label:")
+                         textInput("xlab_text", "X-axis label:")
             ),
             mainPanel(width = 9,
-                      checkboxInput("scale_opt", label = "Scale data"),
                       plotOutput("main_plot") %>% withSpinner(color="#0dc5c1"),
                       div(style="display:inline-block;",
                           downloadButton("dl_main_plot_ppt", label = "PPT",
@@ -105,11 +71,11 @@ ui <- function(request) {
                           bookmarkButton(label = "Save Session", 
                                          style = "font-size:13px;height:30px;padding:5px;")),
                       br(), br(),
-                      tags$h4("Input Data:"),
-                      DT::DTOutput('data_dt'),
+                      tags$h4("Summary Stats:"),
+                      DT::DTOutput('stats_dt'),
                       br(), br(),
-                      tags$h4("Statistics:"),
-                      DT::DTOutput('stats_dt')
+                      tags$h4("Input Data:"),
+                      DT::DTOutput('data_dt')
             )
         )
         
@@ -128,16 +94,9 @@ server <- function(input, output) {
     # load prism data
     load_data <- reactive({
         if (is_empty(input$input_file)) return(NULL)
-        ext <- sub("^(.+)\\.(pzfx|csv|xlsx)","\\2",input$input_file$name)
-        if (ext == "pzfx") {
-            df <- as.data.frame(read_pzfx(path = input$input_file$datapath))
-        } else if(ext == "csv") {
-            df <- as.data.frame(read.csv(file = input$input_file$datapath))
-        } else if(ext == "xlsx") {
+        ext <- sub("^(.+)\\.(xlsx)","\\2",input$input_file$name)
+        if(ext == "xlsx") {
             df <- as.data.frame(read_excel(path = input$input_file$datapath))
-        }
-        if (max(df, na.rm = T) > 100) {
-            df <- log2(df)
         }
         return(df)
     })
@@ -152,66 +111,19 @@ server <- function(input, output) {
         DT::datatable(as.data.frame(load_data()), 
                       escape = F, 
                       rownames = F,
-                      selection = list(target = 'column'),
                       options = list(dom = 't',
                                      pagelength = 5,
                                      lengthMenu = list(c(5, 10, -1),
-                                                       c('5', '10', 'All'))), 
-                      caption = "Select columns to highlight")
+                                                       c('5', '10', 'All'))))
     )
     
     # run aov
     get_stats <- reactive({
         df <- load_data()
         if (is_empty(df)) return(NULL)
-        statdat <- na.omit(gather(df, "category", "value"))
-        statdat$category <- factor(statdat$category, levels = colnames(df))
-        if (input$stat_choice == "anova") {
-            aov_res <- aov(value~category, data = statdat)
-            tukeys_res <- TukeyHSD(aov_res)
-            tukeys_res <- as.data.frame(tukeys_res$category)
-            pattern <- paste0(colnames(df), collapse = "|")
-            pattern <- paste0("^(", pattern, ")\\-(", pattern, ")$")
-            stat_res <- data.frame(Numerator = sub(pattern, "\\1", rownames(tukeys_res)),
-                                  Denominator = sub(pattern, "\\2", rownames(tukeys_res)),
-                                  Difference = round(tukeys_res$diff,2), 
-                                  CI_lower = round(tukeys_res$lwr,2),
-                                  CI_upper = round(tukeys_res$upr,2),
-                                  p_value = formatC(tukeys_res$`p adj`,
-                                                    format = "e", digits = 2),
-                                  check.names = F)
-        } else if(input$stat_choice == "ttest") {
-            if (input$control_type == "single") {
-                denom_lvl <- colnames(df)[1]
-                stat_res <- lapply(colnames(df)[-1], function(x) {
-                    t_res <- t.test(statdat[statdat$category == x,]$value,
-                                    statdat[statdat$category == denom_lvl,]$value)
-                    data.frame(Numerator = x,
-                               Denominator = denom_lvl,
-                               Difference = round((t_res$estimate[1] - t_res$estimate[2]), 2),
-                               CI_lower = round(t_res$conf.int[1], 2),
-                               CI_upper = round(t_res$conf.int[2], 2),
-                               p_value = formatC(t_res$p.value,
-                                                 format = "e", digits = 2),
-                               check.names = F, row.names = NULL)
-                }) %>% bind_rows()
-            } else if (input$control_type == "paired") {
-                stat_res <- lapply(seq(1, ncol(df), 2), function(i) {
-                    cntrl <- colnames(df)[i]
-                    cond <- colnames(df)[i+1]
-                    t_res <- t.test(statdat[statdat$category == cond,]$value,
-                                    statdat[statdat$category == cntrl,]$value)
-                    data.frame(Numerator = x,
-                               Denominator = denom_lvl,
-                               Difference = round((t_res$estimate[1] - t_res$estimate[2]), 2),
-                               CI_lower = round(t_res$conf.int[1], 2),
-                               CI_upper = round(t_res$conf.int[2], 2),
-                               p_value = formatC(t_res$p.value,
-                                                 format = "e", digits = 2),
-                               check.names = F, row.names = NULL)
-                }) %>% bind_rows()
-            }
-        }
+        statdat <- na.omit(gather(df, "condition", "value"))
+        statdat$condition <- factor(statdat$condition, levels = colnames(df))
+        
         return(stat_res)
     })
     
@@ -262,59 +174,59 @@ server <- function(input, output) {
         ymin <- min(plotdat$value, na.rm = T)
         yrange <- ymax-ymin
         p <- ggplot(plotdat, aes(x = category, y = value)) +
-          coord_cartesian(clip = "off", expand = c(1,0), 
-                          ylim = c(floor(ymin - (yrange/20)),
-                                   ceiling(ymax + (yrange/20)))) +
-          scale_fill_manual(values = c(input$col_other, input$col_selected),
-                            name = input$selected_cond_label) +
-          ylab(input$ylab_text) +
-          theme(panel.grid = element_blank(),
-                panel.background = element_blank(),
-                panel.border = element_blank(),
-                axis.ticks.x = element_blank(),
-                axis.ticks.y = element_line(size = .8, color = "black"),
-                axis.ticks.length = unit(.25, "cm"),
-                axis.line.y = element_line(size = .8, color = "black"),
-                axis.line.x = element_line(size = .8, color = "black"),
-                axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1),
-                axis.title.x = element_blank(),
-                axis.text = element_text(color = "black", family = "Arial", size = 18),
-                text = element_text(color = "black", family = "Arial", size = 18),
-                plot.margin = unit(c(5,15,2,2), "lines"),
-                legend.position = c(1.2,.5))
+            coord_cartesian(clip = "off", expand = c(1,0), 
+                            ylim = c(floor(ymin - (yrange/20)),
+                                     ceiling(ymax + (yrange/20)))) +
+            scale_fill_manual(values = c(input$col_other, input$col_selected),
+                              name = input$selected_cond_label) +
+            ylab(input$ylab_text) +
+            theme(panel.grid = element_blank(),
+                  panel.background = element_blank(),
+                  panel.border = element_blank(),
+                  axis.ticks.x = element_blank(),
+                  axis.ticks.y = element_line(size = .8, color = "black"),
+                  axis.ticks.length = unit(.25, "cm"),
+                  axis.line.y = element_line(size = .8, color = "black"),
+                  axis.line.x = element_line(size = .8, color = "black"),
+                  axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1),
+                  axis.title.x = element_blank(),
+                  axis.text = element_text(color = "black", family = "Arial", size = 18),
+                  text = element_text(color = "black", family = "Arial", size = 18),
+                  plot.margin = unit(c(5,15,2,2), "lines"),
+                  legend.position = c(1.2,.5))
         
         # different plot type elements
         if (input$plot_type == "dotplot") {
-          p <- p + 
-            geom_dotplot(aes(fill = selected),
-                         bins = 50,
-                         binaxis='y', stackdir='center', 
-                         dotsize = (1.2 + 1/yrange),
-                         lwd = .6, alpha = .8) +
-            stat_summary(fun.y = mean, geom = "tile",
-                         width = .3, height = (yrange/75), 
-                         fill = "black") +
-            stat_summary(fun.y = mean,
-                         fun.ymin = function(x) mean(x) - sd(x),
-                         fun.ymax = function(x) mean(x) + sd(x),
-                         geom = "errorbar", width = .2,  height = .15) 
+            p <- p + 
+                geom_dotplot(aes(fill = selected),
+                             bins = 50,
+                             binaxis='y', stackdir='center', 
+                             dotsize = (1.2 + 1/yrange),
+                             lwd = .6, alpha = .8) +
+                stat_summary(fun.y = mean, geom = "tile",
+                             width = .3, height = (yrange/75), 
+                             fill = "black") +
+                stat_summary(fun.y = mean,
+                             fun.ymin = function(x) mean(x) - sd(x),
+                             fun.ymax = function(x) mean(x) + sd(x),
+                             geom = "errorbar", width = .2,  height = .15) 
         } else if (input$plot_type == "violin") {
-          p <- p + 
-            geom_violin(aes(fill = selected))
+            p <- p + 
+                geom_violin(aes(fill = selected))
         } else if (input$plot_type == "boxplot") {
-          p <- p + 
-            geom_boxplot(aes(fill = selected), width = .75)
+            p <- p + 
+                geom_boxplot(aes(fill = selected), width = .75)
         }
         
         # different significance label elements
         if (input$sig_style == "bars") {
-          stat_res <- get_stats()[input$stats_dt_rows_selected,]
-          comps <- as.list(split(stat_res[,1:2], seq(nrow(stat_res))))
-          comps <- unname(lapply(comps, function(x) unname(as.character(x))))
-          p <- p + ggpubr::stat_compare_means(comparisons = comps, 
-                                              label = "p.format",
-                                              size = 6,
-                                              bracket.size = 1.1)
+            stat_res <- get_stats()[input$stats_dt_rows_selected,]
+            comps <- as.list(split(stat_res[,1:2], seq(nrow(stat_res))))
+            comps <- unname(lapply(comps, function(x) unname(as.character(x))))
+            p <- p + ggpubr::stat_compare_means(comparisons = comps, 
+                                                label = "p.format",
+                                                size = 6,
+                                                bracket.size = 1.1)
         } else if (input$sig_style == "heatmap") {
             sigdat <- get_stats()
             sigdat$p_value <- as.numeric(sigdat$p_value)
@@ -342,7 +254,7 @@ server <- function(input, output) {
                 numer_lvl <- colnames(df)[seq(2, ncol(df), 2)]
                 matched_idx <- sapply(seq_along(numer_lvl), function(x) {
                     which(sigdat$Denominator == denom_lvl[x] &
-                          sigdat$Numerator == numer_lvl[x]) })
+                              sigdat$Numerator == numer_lvl[x]) })
                 sigdat <- sigdat[matched_idx,]
                 sigdat$x <- sapply(sigdat$Numerator, function(x) which(colnames(df) == x) - .5)
                 sigdat$y <- ceiling(ymax + (yrange/20)) + (yrange/20)
